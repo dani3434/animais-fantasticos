46d3d1e37a33dcff70a18900fc430e846534bc24
Daniel Dias <danieldias6946@gmail.com>
2021-08-18 18:08:13 -0300

    concertando o animais-api.json

--- js/main.js
+++ js/main.js
@@ -35,5 +35,5 @@ menuMobile.init();
 
 const funcionamento = new Funcionamento('[data-semana]', 'aberto');
 funcionamento.init();
-FetchAnimais('../../animais-api.json', '.numeros-grid');
+FetchAnimais('./animais-api.json', '.numeros-grid');
 FetchBitcon('https://blockchain.info/ticker', '.btc-preco');
